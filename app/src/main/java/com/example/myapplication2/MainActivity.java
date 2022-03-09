d2231160e42d8eec0be99c386bb8b22d97dde00e
sagar <sagarpatel180496@gmail.com>
2022-03-08 18:24:40 -0800

    Added new feature in new version

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -8,17 +8,28 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-  Button btn;
+  Button btn,bt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.button1);
+        bt2=findViewById(R.id.button2);
         btn.setOnClickListener(this);
+        bt2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+
+        switch (view.getId())
+        {
+
+            case R.id.button1:
+                Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+            case R.id.button2:
+                Toast.makeText(this, "hello2", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
